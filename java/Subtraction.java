4651c9d4e0e5602cce402c8ed611cf0a00c9df21
Sowmika <sowmika.perinbanayagam@gmail.com>
2024-11-30 19:54:05 +0530

    print multiplication of two digit

---
+++ java/Subtraction.java
@@ -0,0 +1,16 @@
+package Practice_programming.java;
+
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String [] args)
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the firstNumber:");
+    int firstNumber = scanner.nextInt();
+    System.out.println("Enter the secondNumber:");
+    int secondNumber = scanner.nextInt();
+    int output = firstNumber-secondNumber;
+    System.out.println("Enter the number: +firstNumber-secondNumber");
+    scanner.close();
+    
+}

091b208cca175cab090e9121badaf5a289390c57
Sowmika <sowmika.perinbanayagam@gmail.com>
2024-11-30 19:41:33 +0530

    Mutiplication of two digits

---
+++ multiplication.java
@@ -0,0 +1,17 @@
+package Practice_programming;
+
+import java.util.Scanner;
+
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the firstNumber: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the secondNumber: ");
+        int secondNumber = scanner.nextInt();
+        int output = firstNumber * secondNumber;
+        System.out.println("Enter the number:" + output);
+        scanner.close();
+    }
+
+}

3a21c2cad975bf488c7dd35ff5dc893b1c5ce6ba
Sowmika <sowmika.perinbanayagam@gmail.com>
2024-12-01 13:04:08 +0530

    print the 10values in single array

---
+++ java/array.java
@@ -0,0 +1,12 @@
+package Practice_programming.java;
+
+public class array {
+    public static void main(String[] args) {
+        int arr[] = { 2, 9, 8, 6, 5, 7, 4, 6, 3, 4 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("The element" + i + ":" + arr[i]);
+
+        }
+
+    }
+}
